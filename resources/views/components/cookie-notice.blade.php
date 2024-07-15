802b7d8716cbca1478b4718999af95628f3ad171
leon-hub101 <L.Pretorius07@gmail.com>
2024-07-15 17:54:06 +0200

    Added cookie-notice and footer blade pages

---
+++ resources/views/components/cookie-notice.blade.php
@@ -0,0 +1,4 @@
+<div id="cookie-notice" style="background-color: #f8f9fa; padding: 10px; text-align: center; position: fixed; bottom: 0; width: 100%;">
+    <p>We use cookies to improve your experience. By using our site, you agree to our <a href="{{ url('/legal') }}">Privacy Policy</a>.</p>
+    <button onclick="document.getElementById('cookie-notice').style.display='none';">Got it!</button>
+</div>
---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,7 @@
+<footer style="background-color: #f8f9fa; padding: 10px; text-align: center; position: fixed; bottom: 0; width: 100%;">
+    <p>
+        <a href="{{ url('/search') }}">Search</a> |
+        <a href="{{ url('/legal') }}">Legal</a>
+    </p>
+    <p>Copyright &copy; {{ date('Y') }} ArticleManagement App. All rights reserved.</p>
+</footer>
