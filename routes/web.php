d1fd2e80d46ac2d15f1577bc3237b836cd46c702
leon-hub101 <L.Pretorius07@gmail.com>
2024-07-15 15:50:23 +0200

    Deleted redundant code from route file

--- routes/web.php
+++ routes/web.php
@@ -46,24 +46,3 @@ Route::get('/tag/{slug}', function ($slug) {
 Route::get('/legal', function () {
     return view('legal');
 });
-
-/* Default welcome page
-Route::get('/welcome', function () {
-    return view('welcome');
-});
-
-// Dashboard route
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-/* Profile routes
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-// Auth routes
-require __DIR__.'/auth.php';
-*/
