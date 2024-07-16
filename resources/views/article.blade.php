850f44cbb7571330dcbd128254edf826875838c1
leon-hub101 <L.Pretorius07@gmail.com>
2024-07-16 08:55:08 +0200

    Formatted styling on my blade pages

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -3,22 +3,22 @@
 @section('title', $article->title)
 
 @section('content')
-    <!-- Display the article title -->
-    <h1 class="my-4">{{ $article->title }}</h1>
+<!-- Display the article title -->
+<h1 class="my-4">{{ $article->title }}</h1>
 
-    <!-- Display the category of the article -->
-    <p><strong>Category:</strong> {{ $article->category->name }}</p>
+<!-- Display the category of the article -->
+<p><strong>Category:</strong> {{ $article->category->name }}</p>
 
-    <!-- Display the tags associated with the article -->
-    <p><strong>Tags:</strong>
-        @foreach($article->tags as $tag)
-            <span class="badge badge-primary">{{ $tag->name }}</span>
-        @endforeach
-    </p>
+<!-- Display the tags associated with the article -->
+<p><strong>Tags:</strong>
+    @foreach($article->tags as $tag)
+        <span class="badge badge-primary">{{ $tag->name }}</span>
+    @endforeach
+</p>
 
-    <!-- Display the creation date of the article -->
-    <p><strong>Created on:</strong> {{ $article->creation_date }}</p>
+<!-- Display the creation date of the article -->
+<p><strong>Created on:</strong> {{ $article->creation_date }}</p>
 
-    <!-- Display the content of the article -->
-    <div>{{ $article->content }}</div>
+<!-- Display the content of the article -->
+<div>{{ $article->content }}</div>
 @endsection
